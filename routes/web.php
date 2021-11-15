348e5704b716c2a48f4cdeea005f396d1ea6072c
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-11-15 17:44:44 +0800

    初步建置三個路由(Routes)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+Route::post('/task', function(Request $request){
+    //
+});
+
+Route::delete('/task/{task}', function(Task $task){
+    //
 });
